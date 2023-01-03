13c3e5623faf5ae9cb75ec6c6f8afec079f3ca6f
Pratikshit <pratikshit@icloud.com>
2023-01-04 00:28:22 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Pratikshit";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Catalog App")),
+      body: Center(
+        child: Container(
+            child: Text("welcomeee to $days days of flutter by $name")),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
