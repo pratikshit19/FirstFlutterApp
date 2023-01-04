3988a8dbd9000732ac48762ad2ebefb7c6ad2c7d
Pratikshit <pratikshit@icloud.com>
2023-01-04 22:27:53 +0530

    Day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,19 +1,62 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",style: TextStyle(
-          fontSize: 35,
-          color: Colors.blueGrey,
-          fontWeight: FontWeight.bold,
-        ),
-        )
-      )
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_img.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  TextField(
+                   decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                 ),
+               ),
+                  TextField(
+                    obscureText: true,
+                   decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                ),
+              ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Pratikshit!");
+                    },
+                    child: Text("Login"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
